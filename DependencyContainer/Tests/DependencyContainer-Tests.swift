f030141fb7526052327ff69720b9f40389f9a911
Maxim Bazarov <bazaroffma@gmail.com>
2022-10-02 21:58:14 +0200

    add Dependency Container initial implementation

--- DependencyContainer/Tests/DependencyContainer-Tests.swift
+++ DependencyContainer/Tests/DependencyContainer-Tests.swift
@@ -14,4 +14,51 @@
 //===----------------------------------------------------------------------===//
 //
 
-import Foundation
+import XCTest
+@testable import DependencyContainer
+
+private protocol Interface: AnyObject {}
+private final class InterfaceMock: Interface {}
+
+@MainActor
+final class DependencyContainer_Tests: XCTestCase {
+    
+    func test_Injected_then_returnsDefaultInstance() throws {
+        let defaultInstance = InterfaceMock()
+        @Injected(as: Interface.self) var sut = { defaultInstance }
+        
+        XCTAssertEqual(ObjectIdentifier(defaultInstance), ObjectIdentifier(sut))
+    }
+    
+    func test_Injected_Global_when_injectGlobal_then_returnsInjectedInstance() throws {
+        let defaultInstance = InterfaceMock()
+        let injectedInstance = InterfaceMock()
+        @Injected(as: Interface.self) var sut = { defaultInstance }
+        
+        DependencyContainer.default.injectGlobal(injectedInstance, of: Interface.self)
+        
+        XCTAssertEqual(ObjectIdentifier(injectedInstance), ObjectIdentifier(sut))
+    }
+    
+    func test_Injected_Global_when_injectForConsumer_then_returnsDefault() throws {
+        let defaultInstance = InterfaceMock()
+        let injectedInstance = InterfaceMock()
+        @Injected(as: Interface.self) var sut = { defaultInstance }
+        
+        DependencyContainer.default
+            .inject(injectedInstance, of: Interface.self, for: Self.self)
+        
+        XCTAssertEqual(ObjectIdentifier(defaultInstance), ObjectIdentifier(sut))
+    }
+    
+    func test_Injected_ForConsumer_when_injectForConsumer_then_returnsInjected() throws {
+        let defaultInstance = InterfaceMock()
+        let injectedInstance = InterfaceMock()
+        @Injected(as: Interface.self, into: Self.self) var sut = { defaultInstance }
+        
+        DependencyContainer.default
+            .inject(injectedInstance, of: Interface.self, for: Self.self)
+        
+        XCTAssertEqual(ObjectIdentifier(injectedInstance), ObjectIdentifier(sut))
+    }
+}
