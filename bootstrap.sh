8ea85683b1bd8a1ad97b6819b05bd5de25de9358
Maxim Bazarov <bazaroffma@gmail.com>
2022-10-03 10:05:48 +0200

    add bootstrap, restructure folders, move effect bus to Decore

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 IDETemplateMacrosTarget=".swiftpm/xcode/package.xcworkspace/xcshareddata/IDETemplateMacros.plist"
 IDETemplateMacrosSource="./IDETemplateMacros.plist"
 
-# Copy template for new files to include license.
+# Copy template for new files to include copyright header.
 cp $IDETemplateMacrosSource $IDETemplateMacrosTarget
